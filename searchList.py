ebc8f3d4d9f7b5f3d0f83af3d6422557436ede5b
rozalski <anfx@mail.ru>
2018-11-12 20:49:49 +0300

    Вхождение элементов списка в строку

--- searchList.py
+++ searchList.py
@@ -1,9 +1,13 @@
 #!./env/bin/python3.5
 with open('stop_words', 'r') as stopFile: stopWords = stopFile.read().replace('\n', ',').split(',')
-testStr = 'Строка в которой будем искать'
-for word in stopWords:
-    if word in testStr:
-        print('Найдено совпадение')
-    else:
-        print('Совпадений не найдено')
 print(stopWords)
+stopWords = list(filter(None, stopWords))
+print(stopWords)
+testStr = 'Строка в которой будем искать'
+res = any(word in testStr for word in stopWords)
+print(res)
+#for word in stopWords:
+#    if word in testStr:
+#        print('Найдено совпадение')
+#    else:
+#        print('Совпадений не найдено')
